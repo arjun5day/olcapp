cc29b267dc5cf4bf90ecdac044a011fed14d4d6b
arjun5day <arjun5day@users.noreply.github.com>
2018-07-15 01:47:52 +1000

    Update database.php

--- olc_app/includes/database.php
+++ olc_app/includes/database.php
@@ -1,5 +1,5 @@
 <?php
-$connection = mysqli_connect("localhost", "arjun_olc", "arjunolc", "arjun_olc");
+$connection = mysqli_connect("localhost", "root", "", "db_olc");
 	if (mysqli_connect_errno()){
 		echo "Failed to connect to MySQL : " . mysqli_connect_error();
 	}
